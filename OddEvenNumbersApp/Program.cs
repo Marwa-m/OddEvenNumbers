a0fb733117992a50165dda8ea2f4cc14f3e8c81b
Marwa Waled Almahmoud <marwa.almahmoud@hotmail.com>
2024-07-28 05:43:01 +0300

    initial commit

---
+++ OddEvenNumbersApp/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+PrintNumbers("Numbers", numbers);
+PrintNumbers("Even Numbers", numbers.Where(x => IsEven(x)));
+
+PrintNumbers("Odd Numbers", numbers.Where(x => IsOdd(x)));
+Console.ReadKey();
+
+void PrintNumbers(string title, IEnumerable<int> numbers)
+{
+    Console.WriteLine();
+    Console.WriteLine(title);
+    Console.Write("[");
+    foreach (int number in numbers)
+        Console.Write($" {number} ,");
+    Console.Write("]");
+    Console.WriteLine();
+}
+bool IsEven(int number)
+{
+    if (number % 2 == 0)
+        return true;
+    else
+        return false;
+
+}
+bool IsOdd(int number)
+{
+    if (number % 2 != 0)
+        return true;
+    else
+        return false;
+
+}
